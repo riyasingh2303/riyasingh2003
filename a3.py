26787f9573b15df265b46f63064d84d87e137f69
riyasingh2303 <111672250+riyasingh2303@users.noreply.github.com>
2022-08-21 09:47:29 +0530

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,58 @@
+Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: /Users/Ajeet/PYTHON/assignment-3.py =================
+"RIYA"+"SINGH"
+'RIYASINGH'
+ord("m")
+109
+char(100)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    char(100)
+NameError: name 'char' is not defined. Did you mean: 'chr'?
+char("100")
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    char("100")
+NameError: name 'char' is not defined. Did you mean: 'chr'?
+chr(100)
+'d'
+x = 30
+bin(x)
+'0b11110'
+x = 35
+oct(x)
+'0o43'
+x = 25
+hex(x)
+'0x19'
+a = 1100101
+dec(a)
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    dec(a)
+NameError: name 'dec' is not defined
+decimal(a)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    decimal(a)
+NameError: name 'decimal' is not defined
+oct(a)
+'0o4144505'
+deci(a)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    deci(a)
+NameError: name 'deci' is not defined
+de(a)
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    de(a)
+NameError: name 'de' is not defined
+d(a)
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    d(a)
+NameError: name 'd' is not defined. Did you mean: 'id'?
+r = 24
